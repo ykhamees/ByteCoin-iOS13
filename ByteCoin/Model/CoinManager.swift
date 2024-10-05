dd3a42a86afa3909d8d0e899ee4bc90c843963cb
Yehia Khamis <yehia.khamees@gmail.com>
2024-10-05 10:18:14 +0300

    Update CoinManager.swift

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -13,7 +13,7 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "A398CF98-7407-4EC9-ABF5-C683BECC3CD4"
+    let apiKey = "####"
     
     let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
     
